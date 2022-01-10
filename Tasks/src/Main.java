a5a0c185cb76c9922272ef84cf781eed2cc61256
Maria Şuteu <mara.suteu@gmail.com>
2022-01-10 19:38:23 +0200

    Tasks Main unfinished

---
+++ Tasks/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    Task[] listTasks;
+    FileInputStream file;
+    {
+        try {
+            file = new FileInputStream("offerten.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    Scanner sc = new Scanner(file);
+    int index = 0;
+    while(sc.hasNextLine()){
+        String[] objektData = sc.NextLine().split("$");
+
+        if (objektData[4] instanceof Enum<Task.Status>){
+        Task newTask = new Task(Integer.parseInt(objektData[0]), objektData[1],
+                Integer.parseInt(objektData[2]), Integer.parseInt(objektData[3]), objektData[4]);
+
+        listTasks.add(index, newTask);
+    }
+
+
+}
