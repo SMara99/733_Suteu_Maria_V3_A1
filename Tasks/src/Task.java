c21cbc816225ece646ce8cf2f9c2b46fd78b51b0
Maria Şuteu <mara.suteu@gmail.com>
2022-01-10 19:04:32 +0200

    class Task

---
+++ Tasks/src/Task.java
@@ -0,0 +1,49 @@
+public class Task {
+    public enum Status{
+        Open, In_Progress, Done
+    }
+    int id;
+    String taskName;
+    int hoursBudget;
+    int hoursSpent;
+    Status status;
+
+    public Task(int id, String taskName, int hoursSpent, int hoursBudget, Status status){
+        this.id = id;
+        this.hoursSpent = hoursSpent;
+        this.hoursBudget = hoursBudget;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public int getHoursBudget() {
+        return hoursBudget;
+    }
+
+    public int getHoursSpent() {
+        return hoursSpent;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public void setHoursBudget(int hoursBudget) {
+        this.hoursBudget = hoursBudget;
+    }
+
+    public void setHoursSpent(int hoursSpent) {
+        this.hoursSpent = hoursSpent;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
